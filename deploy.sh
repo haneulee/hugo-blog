74b5b6345522578c2d718a621d4a7307997af294
Haneul Lee <lovesky4294@gmail.com>
2021-05-13 16:00:45 +0900

    rebuilding site 2021년 5월 13일 목요일 16시 00분 45초 KST

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-# If a command fails then the deploy stops
-set -e
-
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-tranquilpeak-theme
 
 # Go To Public folder
 cd public
-
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
 fi
 git commit -m "$msg"
 
 # Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
+
+
+# blog 저장소 Commit & Push
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
 git push origin master
